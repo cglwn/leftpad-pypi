7184e2cd37256058cbece2aeebf4bff2533bd699
James Cagalawan <james.cagalawan@gmail.com>
2018-07-21 22:43:17 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="leftpad",
-    version="0.1.1",
+    version="0.1.2",
     author="James Cagalawan",
     author_email="james.cagalawan@gmail.com",
     description="A port of the infamous left-pad npm package",
